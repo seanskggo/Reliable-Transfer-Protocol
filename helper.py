dd32c2d0c8601e624ce08eba49737017f01a5c9b
Sean <seanskg72@gmail.com>
2021-07-30 16:26:02 +1000

    time moved

--- helper.py
+++ helper.py
@@ -81,6 +81,10 @@ class TCP:
         if packet_type in [Packet.FIN, Packet.FINACK, Packet.SYN, Packet.SYNACK]: self.seq += 1
         else: self.seq += len(data)
 
+    # Get time elapsed
+    def get_time(self) -> float:
+        return round((time.time() - self.epoch) * 1000, 3)
+
     # Get current log
     def get_log(self) -> list:
         return self.log
@@ -97,29 +101,24 @@ class Receiver(TCP):
     def send(self, data, packet_type) -> None:
         pkt = struct.pack(f"!II{self.MSS}sII2s", 
             *self.encoder([self.seq, self.ack, data, self.MSS, self.MWS, packet_type]))
-        ttime = round((time.time() - self.epoch) * 1000, 3)
-        self.log.append([Action.SEND, ttime, packet_type, self.seq, self.ack, len(data)])
+        self.log.append([Action.SEND, self.get_time(), packet_type, self.seq, self.ack, len(data)])
         self.increment_seq(packet_type, data)
         self.server.sendto(pkt, self.addr)
 
     def receive_opening(self) -> None:
         msg, addr = self.server.recvfrom(self.HEADER_SIZE)
-        ttime = round((time.time() - self.epoch) * 1000, 3)
-        seq, ack, data, MSS, MWS, packet_type = self.decoder(struct.unpack("!II0sII2s", msg))
-        self.log.append([Action.RECEIVE, ttime, packet_type, seq, ack, len(data)])
-        self.increment_seq(packet_type, data)
+        self.ack, rcv_seq, data, MSS, MWS, packet_type = self.decoder(struct.unpack("!II0sII2s", msg))
+        self.log.append([Action.RECEIVE, self.get_time(), packet_type, self.ack, rcv_seq, len(data)])
+        self.increment_ack(packet_type, data)
         self.MSS = MSS
         self.MWS = MWS
         self.addr = addr
-        self.ack = seq
 
     def receive(self) -> set:
         msg, _ = self.server.recvfrom(self.MSS + self.HEADER_SIZE)
-        ttime = round((time.time() - self.epoch) * 1000, 3)
-        seq, ack, data, _, _, packet_type = self.decoder(struct.unpack(f"!II{self.MSS}sII2s", msg))
-        self.log.append([Action.RECEIVE, ttime, packet_type, seq, ack, len(data)])
-        self.increment_seq(packet_type, data)
-        self.ack = seq
+        self.ack, rcv_seq, data, _, _, packet_type = self.decoder(struct.unpack(f"!II{self.MSS}sII2s", msg))
+        self.log.append([Action.RECEIVE, self.get_time(), packet_type, self.ack, rcv_seq, len(data)])
+        self.increment_ack(packet_type, data)
         return (data, packet_type)
 
 class Sender(TCP):
@@ -133,26 +132,22 @@ class Sender(TCP):
     def send_opening(self) -> None:
         pkt = struct.pack("!II0sII2s", 
             *self.encoder([self.seq, self.ack, Packet.NONE, self.MSS, self.MWS, Packet.SYN]))
-        ttime = round((time.time() - self.epoch) * 1000, 3)
-        self.log.append([Action.SEND, ttime, Packet.SYN, self.seq, self.ack, len(Packet.NONE)])
+        self.log.append([Action.SEND, self.get_time(), Packet.SYN, self.seq, self.ack, len(Packet.NONE)])
         self.increment_seq(Packet.SYN, Packet.NONE)
         self.client.sendto(pkt, self.addr)
 
     def send(self, data, packet_type):
         pkt = struct.pack(f"!II{self.MSS}sII2s", 
             *self.encoder([self.seq, self.ack, data, self.MSS, self.MWS, packet_type]))
-        ttime = round((time.time() - self.epoch) * 1000, 3)
-        self.log.append([Action.SEND, ttime, packet_type, self.seq, self.ack, len(data)])
+        self.log.append([Action.SEND, self.get_time(), packet_type, self.seq, self.ack, len(data)])
         self.client.sendto(pkt, self.addr)
         self.increment_seq(Packet.DATA, data)
 
     def receive(self) -> None:
         msg, _ = self.client.recvfrom(self.MSS + self.HEADER_SIZE)
-        ttime = round((time.time() - self.epoch) * 1000, 3)
-        seq, ack, data, _, _, packet_type = self.decoder(struct.unpack(f"!II{self.MSS}sII2s", msg))
-        self.log.append([Action.RECEIVE, ttime, packet_type, seq, ack, len(data)])
-        self.increment_seq(packet_type, data)
-        self.ack = seq
+        self.ack, rcv_seq, data, _, _, packet_type = self.decoder(struct.unpack(f"!II{self.MSS}sII2s", msg))
+        self.log.append([Action.RECEIVE, self.get_time(), packet_type, self.ack, rcv_seq, len(data)])
+        self.increment_ack(packet_type, data)
 
 # TCP Window class
 class Window:
