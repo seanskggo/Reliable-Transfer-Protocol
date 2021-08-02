8209981079bbfeb899a19a40fd37d20b63a7750d
Sean <seanskg72@gmail.com>
2021-08-02 17:26:35 +1000

    restart

---
+++ _receiver.py
@@ -0,0 +1,69 @@
+##################################################################
+# COMP3331/9331 Computer Networks and Applications 
+# Assignment 2 | Term 2, 2021
+# By Sean Go (z5310199)
+#
+# >>> Python Verion Used: 3.8.5
+##################################################################
+
+##################################################################
+# Imports
+##################################################################
+
+import sys
+import socket
+from helper import *
+
+##################################################################
+# Constants
+##################################################################
+
+IP = '127.0.0.1'
+RECEIVER_ERROR = \
+    'USAGE: python receiver.py receiver_port FileReceiverd.txt'
+
+##################################################################
+# PTP
+##################################################################
+
+# Parse commandline arguments
+if (len(sys.argv) != 3): exit(RECEIVER_ERROR)
+try: port, filename = int(sys.argv[1]), sys.argv[2]
+except: exit(RECEIVER_ERROR)
+
+# Set initial sequence number and ack
+seq, ack = 154, 0
+
+# Create UDP socket server
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server.bind((IP, port))
+receiver = Receiver(server, seq, ack)
+
+# Opening handshake -> no connection or teardown packets will be dropped
+# Received and sets the MSS for the TCP connection
+window = receiver.receive_opening()
+receiver.send_ack(Packet.NONE, Packet.SYNACK)
+receiver.receive()
+
+# Open and write to file until teardown
+with open(filename, "w") as file:
+    while True:
+        data, packet_type = receiver.receive()
+        # Handle teardown -> no connection or teardown packets will be dropped
+        if packet_type == Packet.FIN: break
+        receiver.send_ack(Packet.NONE, Packet.ACK)
+        file.write(data)
+    receiver.send_ack(Packet.NONE, Packet.FINACK)
+    receiver.receive()
+
+# Create log file
+with open("Receiver_log.txt", "w") as logfile:
+    tot_data, num_seg, num_dup = [0] * 3
+    for a, b, c, d, e, f in receiver.get_log():
+        if a == Action.RECEIVE: tot_data += f
+        if a == Action.RECEIVE and c == Packet.DATA: num_seg += 1
+        logfile.write(f"{a:<5} {b:<12} {c:<6} {d:<6} {f:<6} {e:<6}\n")
+    logfile.write("\n--------- Log File Statistics ---------\n\n")
+    logfile.write(f"Total Data Received (bytes):     {tot_data}\n")
+    logfile.write(f"No. Data Segments Received:      {num_seg}\n")
+    logfile.write(f"No. Duplicate Segments:          {num_dup}\n")
---
+++ _sender.py
@@ -0,0 +1,103 @@
+##################################################################
+# COMP3331/9331 Computer Networks and Applications 
+# Assignment 2 | Term 2, 2021
+# By Sean Go (z5310199)
+#
+# >>> Python Verion Used: 3.8.5
+##################################################################
+
+##################################################################
+# Imports
+##################################################################
+
+import sys
+import socket
+from helper import *
+
+##################################################################
+# Constants
+##################################################################
+
+SENDER_ERROR = \
+    'USAGE: python sender.py receiver_host_ip receiver_port ' \
+    + 'FileToSend.txt MWS MSS timeout pdrop seed'
+PDROP_ERROR = 'Pdrop parameter must be between 0 and 1'
+MSS_ERROR = 'Maximum Segment Size must be greater than 0'
+
+##################################################################
+# PTP
+##################################################################
+
+# Parse commandline arguments
+if (len(sys.argv) != 9): exit(SENDER_ERROR)
+try:
+    ip, port, filename, MWS, MSS, timeout, pdrop, seed = (
+        sys.argv[1], int(sys.argv[2]), 
+        sys.argv[3], int(sys.argv[4]), 
+        int(sys.argv[5]), int(sys.argv[6]), 
+        float(sys.argv[7]), sys.argv[8], 
+    )
+except: exit(SENDER_ERROR)
+
+# Basic error handling
+if not 0 < pdrop < 1: exit(PDROP_ERROR)
+if MSS <= 0: exit(MSS_ERROR)
+
+# Set initial sequence number and ack
+seq, ack = 121, 0
+
+# Create UDP socket client
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.settimeout(timeout/1000)
+sender = Sender(client, (ip, port), MSS, MWS, seq, ack)
+sender.set_PL_module(seed, pdrop)
+
+# Opening handshake -> no connection or teardown packets will be dropped
+sender.send_opening(Packet.SYN)
+sender.receive(handshake=True)
+sender.send(Packet.NONE, Packet.ACK, use_PL=False, handshake=True)    
+
+# Open file for reading. If the file does not exist, throw error
+with open(filename, "r") as file:
+    packet = file.read(MSS)
+    while packet:
+        print("-------")
+        for i in range(int(MWS/MSS)): 
+            sender.send(packet, Packet.DATA, use_PL=False)
+            packet = file.read(MSS)
+            if not packet: break
+        try: [sender.receive() for _ in range(i + 1)]
+        except: print("timeout")
+    print("-------")
+    # Initiate teardown -> no connection or teardown packets will be dropped
+    sender.send(Packet.NONE, Packet.FIN, use_PL=False, handshake=True)
+    sender.receive(handshake=True)
+    sender.send(Packet.NONE, Packet.ACK, use_PL=False, handshake=True)
+
+# Create log file
+with open("Sender_log.txt", "w") as logfile:
+    tot_data, num_seg, drp_pkt, re_seg, dup_ack = [0] * 5
+    for a, b, c, d, e, f in sender.get_log():
+        if a == Action.SEND: tot_data += f
+        if a == Action.SEND and c == Packet.DATA: num_seg += 1
+        logfile.write(f"{a:<5} {b:<12} {c:<6} {d:<6} {f:<6} {e:<6}\n")
+    logfile.write("\n--------- Log File Statistics ---------\n\n")
+    logfile.write(f"Total Data Transferred (bytes):  {tot_data}\n")
+    logfile.write(f"No. Data Segments Sent:          {num_seg}\n")
+    logfile.write(f"No. Packets Dropped:             {drp_pkt}\n")
+    logfile.write(f"No. Retransmitted Segments:      {re_seg}\n")
+    logfile.write(f"No. Duplicate Acknowledgements:  {dup_ack}\n")
+
+##################################################################
+# Important Ideas
+##################################################################
+
+# def send_packet():
+#     sender.send(packet, Packet.DATA)
+#     sender.receive()
+# def check_timeout():
+#     try: send_packet()
+#     except: check_timeout()
+# while packet:
+#     check_timeout()
+#     packet = file.read(MSS)
